72a0cad565754f2056995083afac708cb070311d
ManoleMadalina <67560247+ManoleMadalina@users.noreply.github.com>
2020-08-13 16:52:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomBtn = document.getElementById("random");
+
+function randomColor1() {
+  var x = Math.floor(Math.random() * 255);
+  var y = Math.floor(Math.random() * 255);
+  var z = Math.floor(Math.random() * 255);
+  var bgColor = "rgb(" + x + ", " + y + ", " + z + ")";
+  return bgColor;
+}
+
+function randomColor2() {
+  var x = Math.floor(Math.random() * 255);
+  var y = Math.floor(Math.random() * 255);
+  var z = Math.floor(Math.random() * 255);
+  var bgColor = "rgb(" + x + ", " + y + ", " + z + ")";
+  return bgColor;
+}
+
+function randomBackground() {
+  body.style.background =
+    "linear-gradient(to right, " + randomColor1() + ", " + randomColor2() + ")";
+  css.textContent = body.style.background + ";";
+}
 
 function setGradient() {
   body.style.background =
@@ -13,3 +36,5 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+randomBtn.addEventListener("click", randomBackground);
